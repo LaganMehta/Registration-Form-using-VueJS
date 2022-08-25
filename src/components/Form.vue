6ff2e52ddf5fd85d5de7223ffe22226609d75090
Lagan Mehta <lm6472@srmist.edu.in>
2022-08-25 13:53:51 +0530

    first commit

---
+++ src/components/Form.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="form">
+    <h1>Welcome to Form Page</h1>
+    <br>
+	<input v-model="message" placeholder="Enter Name" />
+  <br>
+  <p>Your Name is: {{ message }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: ''
+    }
+  }
+}
+</script>
+
+<style>
+@media (min-width: 1024px) {
+  .h1 {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
